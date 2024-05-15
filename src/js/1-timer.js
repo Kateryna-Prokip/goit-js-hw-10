b9ddf1453da8725122466667b5b0de22e73e487e
Kateryna-Prokip <153538553+Kateryna-Prokip@users.noreply.github.com>
2024-05-15 22:57:07 +0300

    hw10

---
+++ src/js/1-timer.js
@@ -0,0 +1,63 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+
+const fieldText = document.querySelector('input[datetime-picker]');
+const startButton = document.querySelector('button [data-start]');
+const fieldDays = document.querySelector('span[data-days]');
+const fieldHours = document.querySelector('span[data-hours]');
+const fieldMinutes = document.querySelector('span[data-minutes]');
+const fieldSeconds = document.querySelector('span[data-seconds]');
+let userSelectedDate;
+
+
+
+addLeadingZero(value) 
+// const str1 = '5';
+
+// console.log(str1.padStart(2, '0'));
+// // Expected output: "05"
+
+// const fullNumber = '2034399002125581';
+// const last4Digits = fullNumber.slice(-4);
+// const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+// console.log(maskedNumber);
+// // Expected output: "************5581"
